6842a8ca62e001f51a5595fc05a45f9bb2f05c89
nachool <nachma.olivia1394@gmail.com>
2025-04-25 15:09:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("Just an ordinary train when i was in japan");
+});
+
+audio.addEventListener("click", function audio() {
+  alert("Yeah, this goes wild in tiktok");
+});
+
+gambar.addEventListener("click", function gambar() {
+  alert("Anyway, here's some cat that i found on the internet");
+});
